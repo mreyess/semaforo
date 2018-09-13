836cdbe6f215a2a5b813898255b3a7c32f296d84
Miguel Angel Reyes Solis <15240607@itleon.edu.mx>
2018-09-13 08:29:37 -0500

    Semaforo con bloqueo de boton

--- Practica_uno.ino
+++ Practica_uno.ino
@@ -25,8 +25,8 @@
 #define pot 0        // Definicion del pin de entrada analogica a utilizar para el potenciometro que manipulara la duracion del foco verde del semaforo vehicular
 #define alarma 8     // DEfinicion del pin de entrada digital a utilizar para la alarma que sonara mientras el paso del peaton este en verde    
 int duracion = 0;    // Definicion de la variable que estipula ael tiempo maximo de duracion que tendra el foco verde del semaforo vehicular
-int tiempo_transcurrido = 0; // Definicion de la variable que guarda el tiempo que ha trancurrido desde que se encendio el foco verde del semaforo vehicular 
-
+int tiempo_transcurrido = 9000; // Definicion de la variable que guarda el tiempo que ha trancurrido desde que se encendio el foco verde del semaforo vehicular 
+boolean boton_permiso = true;
 void setup() {
     
     Serial.begin(9600);
@@ -54,19 +54,26 @@ void setup() {
 void loop() {
   // Lectura del potenciometro para saber cuato tiempo durara el foco verder del semaforo vehicular encendido
   // El valor minimo encendidoson 5 segundos y el maximo son 10
- duracion = (analogRead (pot)*10)+10000;
- Serial.println(duracion);
+ duracion = (analogRead (pot)*5)+5000;
+ //Serial.println(duracion);
+ 
 
       // Miestras el boton no este presionado y su tiempo de encendido transcurrido sea menor al que debe durar entonces que siga encendido
-      while(digitalRead(boton) != LOW && tiempo_transcurrido <= duracion){
+      while(digitalRead(boton) != LOW || tiempo_transcurrido < 10000){
         tiempo_transcurrido += 10;  
         delay(10);
+
+        if(digitalRead(boton) == LOW && tiempo_transcurrido > 10000){
+        break;
+        }
         
         }
+
+                
       // Si se presiono el boton o termino su tiempo de duracion entonces que empiece a parpadear
       digitalWrite(led_sem_v, LOW);
      
-      tiempo_transcurrido = 0;
+      
         for(int i = 0; i <= 3; i++){
             delay(500);
             digitalWrite(led_sem_v, HIGH);
@@ -83,12 +90,14 @@ void loop() {
         digitalWrite(led_p_r, LOW);
         digitalWrite(led_p_v, HIGH);
         digitalWrite(alarma, LOW);
-        delay(10000);
+        Serial.println(duracion);
+        delay(duracion);
       // Una vez terminado el tiempo de cruce peatonal que regrese al paso vehicular
         digitalWrite(led_sem_r, LOW);
         digitalWrite(led_sem_v, HIGH);
         digitalWrite(led_p_v, LOW);
         digitalWrite(led_p_r, HIGH);
         digitalWrite(alarma, HIGH);
+        tiempo_transcurrido = 0;
             
 }
